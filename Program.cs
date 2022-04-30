b0bc70aeee37e6cac9ec8bdd516d74c57650e081
NizarAhamed1998 <93996784+NizarAhamed1998@users.noreply.github.com>
2022-04-30 13:48:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurlyBracesProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> st = new Stack<char>();
+            string braces = Console.ReadLine();
+            char[] array = braces.ToCharArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]=='{'||array[i]=='['||array[i]=='(')
+                {
+                    st.Push(array[i]);
+                }
+                if (array[i]=='}'||array[i]==']'||array[i]==')')
+                {
+                    st.Pop();
+                }
+            }
+            if (st.Count==0)
+            {
+                Console.WriteLine("Balanced");
+            }
+            else
+            {
+                Console.WriteLine("Not balaced");
+            }
+        }
+    }
+}
